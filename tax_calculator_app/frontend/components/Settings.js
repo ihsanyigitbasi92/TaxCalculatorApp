ef92e0351a9ccbca35c34e75b08f1934ee9394e8
ihsanyigitbasi92 <ihsanyigitbasi92@gmail.com>
2026-03-23 16:52:46 +0400

    Add tax_calculator_app/frontend/components/Settings.js

---
+++ tax_calculator_app/frontend/components/Settings.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, Switch } from 'react-native';
+
+export default function Settings() {
+  return (
+    <View>
+      <Text>Settings</Text>
+      <Text>Dark Mode</Text>
+      <Switch />
+    </View>
+  );
+}
