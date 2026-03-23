4f935384a742bdade273aef8a6fc4179ddcb091d
ihsanyigitbasi92 <ihsanyigitbasi92@gmail.com>
2026-03-23 16:52:44 +0400

    Add tax_calculator_app/frontend/components/History.js

---
+++ tax_calculator_app/frontend/components/History.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function History() {
+  return (
+    <View>
+      <Text>History</Text>
+      {/* Display tax calculation history here */}
+    </View>
+  );
+}
